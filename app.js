ab44d43fabaa58bcedd9546ecdccf233cb261458
Jon Topper <jon@scalefactory.com>
2018-07-26 19:42:18 +0100

    Change to app

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict'
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => res.send('<html><b>Hello AGAIN World!</b></html>'))
+app.get('/', (req, res) => res.send('<html><b>Hello once more World!</b></html>'))
 module.exports = app
